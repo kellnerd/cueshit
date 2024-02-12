886fea915afa76d8b8bd46c1e09ef5cd26ae1446
David Kellner <52860029+kellnerd@users.noreply.github.com>
2024-02-12 23:57:20 +0100

    feat(cli): Use a custom enum type for the format parameters

--- cli.ts
+++ cli.ts
@@ -1,16 +1,24 @@
 import {
   Command,
+  EnumType,
   ValidationError,
 } from "https://deno.land/x/cliffy@v1.0.0-rc.3/command/mod.ts";
 import formats from "./formats.ts";
+
+const formatIds = Object.keys(formats);
 
 export const cli = new Command()
   .name("cueshit")
   .version("0.1.0")
   .description("Convert between different cuesheet/chapter/tracklist formats.")
-  .option("-f, --from <format>", "ID of the input format.", { required: true })
-  .option("-t, --to <format>", "ID of the output format.", { required: true })
-  .arguments("<input_path:file>")
+  .type("format", new EnumType(formatIds))
+  .option("-f, --from <format:format>", "ID of the input format.", {
+    required: true,
+  })
+  .option("-t, --to <format:format>", "ID of the output format.", {
+    required: true,
+  })
+  .arguments("<input-path:file>")
   .action(async (options, inputPath) => {
     const inputFormat = formats[options.from];
     const parseCues = inputFormat?.parser;
