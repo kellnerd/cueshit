dfb5895114f6ee13a759de3fe834107b6ac873a1
David Kellner <52860029+kellnerd@users.noreply.github.com>
2024-02-16 23:13:36 +0100

    feat: Parse MusicBrainz track parser listings
    Track artists are currently treated as part of the title.

--- format/musicbrainz_tracklist.ts
+++ format/musicbrainz_tracklist.ts
@@ -1,5 +1,10 @@
-import type { CueFormat, CueFormatter } from "../cuesheet.ts";
-import { getDurationFormatter, TimeUnit } from "../duration.ts";
+import type {
+  Cue,
+  CueFormat,
+  CueFormatter,
+  CueSheetParser,
+} from "../cuesheet.ts";
+import { getDurationFormatter, parseDuration, TimeUnit } from "../duration.ts";
 
 const formatDuration = getDurationFormatter({
   largestUnit: TimeUnit.minutes,
@@ -11,7 +16,35 @@ export const formatMusicBrainzTrack: CueFormatter = function (cue) {
   })`;
 };
 
+export const parseMusicBrainzTrackListing: CueSheetParser = function (listing) {
+  const trackPattern =
+    /((?<position>\d+)?\.\s+)?(?<title>.+)\s+\((?<duration>\d+:\d{2})\)/;
+  const tracks: Cue[] = [];
+  let previousTrack: Cue | undefined = undefined;
+
+  for (const line of listing.split("\n")) {
+    const trackMatch = line.match(trackPattern)?.groups;
+    if (!trackMatch) continue;
+
+    const track: Cue = {
+      title: trackMatch.title,
+      position: parseInt(trackMatch.position),
+      duration: parseDuration(trackMatch.duration),
+      timeOffset: previousTrack
+        ? previousTrack.timeOffset + previousTrack.duration!
+        : 0,
+    };
+    tracks.push(track);
+    previousTrack = track;
+  }
+
+  return {
+    cues: tracks,
+  };
+};
+
 export default {
   name: "MusicBrainz Track Parser Listing",
   formatCue: formatMusicBrainzTrack,
+  parse: parseMusicBrainzTrackListing,
 } as CueFormat;
