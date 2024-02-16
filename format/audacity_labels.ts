9f951f99fed00c9f2a6ec9ab806069dfa69c025d
David Kellner <52860029+kellnerd@users.noreply.github.com>
2024-02-16 23:23:48 +0100

    feat: Parse Audacity label tracks

--- format/audacity_labels.ts
+++ format/audacity_labels.ts
@@ -1,4 +1,5 @@
-import type { CueFormat, CueFormatter } from "../cuesheet.ts";
+import type { CueFormat, CueFormatter, CueSheetParser } from "../cuesheet.ts";
+import { isDefined } from '../utils.ts';
 
 export const formatAudacityLabel: CueFormatter = function (cue) {
   const defaultDuration = 1;
@@ -9,7 +10,24 @@ export const formatAudacityLabel: CueFormatter = function (cue) {
   ].join("\t");
 };
 
+export const parseAudacityLabels: CueSheetParser = function (lines) {
+  return {
+    cues: lines.split("\n").map((line, index) => {
+      const [startTime, endTime, label] = line.split("\t");
+      const timeOffset = parseFloat(startTime);
+      if (!label || isNaN(timeOffset)) return;
+      return {
+        position: index + 1,
+        title: label,
+        timeOffset,
+        duration: parseFloat(endTime) - timeOffset,
+      };
+    }).filter(isDefined),
+  };
+};
+
 export default {
   name: "Audacity Label Track",
   formatCue: formatAudacityLabel,
+  parse: parseAudacityLabels,
 } as CueFormat;
