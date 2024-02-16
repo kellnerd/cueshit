e4957d27e962a1ea504654bdc516b5aece37995c
David Kellner <52860029+kellnerd@users.noreply.github.com>
2024-02-16 21:47:01 +0100

    feat: Format OGM Tools chapters

---
+++ format/ogm_chapters.ts
@@ -0,0 +1,22 @@
+import type { CueFormat, CueFormatter } from "../cuesheet.ts";
+import { getDurationFormatter, padNum, TimeUnit } from "../duration.ts";
+
+const formatTimestamp = getDurationFormatter({
+  largestUnit: TimeUnit.hours,
+  smallestUnit: TimeUnit.subSeconds,
+  padLargestUnit: true,
+  subSecondUnits: 1000, // ms
+});
+
+export const formatOgmChapter: CueFormatter = function (cue) {
+  const chapterNumber = padNum(cue.position, 2);
+  return [
+    `CHAPTER${chapterNumber}=${formatTimestamp(cue.timeOffset)}`,
+    `CHAPTER${chapterNumber}NAME=${cue.title}`,
+  ].join("\n");
+};
+
+export default {
+  name: "OGM Tools Chapters",
+  formatCue: formatOgmChapter,
+} as CueFormat;
--- formats.ts
+++ formats.ts
@@ -2,12 +2,14 @@ import type { CueFormat } from "./cuesheet.ts";
 
 import audacity_labels from "./format/audacity_labels.ts";
 import musicbrainz_tracklist from "./format/musicbrainz_tracklist.ts";
+import ogm_chapters from "./format/ogm_chapters.ts";
 import youtube_description from "./format/youtube_description.ts";
 
 /** Maps format IDs to format specifications. */
 const formats: Record<string, CueFormat | undefined> = {
   "audacity": audacity_labels,
   "musicbrainz": musicbrainz_tracklist,
+  "ogm": ogm_chapters,
   "youtube": youtube_description,
 };
 
